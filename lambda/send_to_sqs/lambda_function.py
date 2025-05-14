653e2cf433e06743e551f6f1f2ac219f0658aace
Rahuloli <140624431+Rahuloli@users.noreply.github.com>
2025-05-15 04:06:04 +0530

    Update lambda_function.py

--- lambda/send_to_sqs/lambda_function.py
+++ lambda/send_to_sqs/lambda_function.py
@@ -7,7 +7,19 @@ QUEUE_URL = os.environ['QUEUE_URL']
 
 def lambda_handler(event, context):
     body = json.loads(event['body'])
-    message = body.get('message', 'Hello from Lambda!')
+    job_id = body.get('job_id')
+    job_name = body.get('job_name')
+
+    if not job_id or not job_name:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': 'Missing job_id or job_name'})
+        }
+
+    message = json.dumps({
+        'job_id': job_id,
+        'job_name': job_name
+    })
 
     response = sqs.send_message(
         QueueUrl=QUEUE_URL,
